78395c32d757ea8d6df0294c82df6a83fc4c63f3
Douglas Reith <douglas@reith.com.au>
2018-03-28 17:48:14 +1100

    Travis build improvements (#14)
    * Trial turning off xdebug
    
    * Remove composer `--dev` warning
    
    * Add composer cache
    
    * Add badges to README
    
    * Add php 7.2
    
    * Using --prefer-lowest means we need to polyfill phpunit 5.0 test case class
    
    * Trial running travis with lowest dependencies
    
    * Check folder structure - to be removed
    
    * Do not need that many lowest deps builds
    
    * Remove debug line
    
    * Not using mongodb in these tests
    
    * Move to simpler bash test for existence
    
    * Attempt to fix the composer update error
    
    * Trial with ignore platform reqs for composer pre-build
    
    * Remove accidentally committed composer change
    
    * Move pre-run composer update
    
    * Move pre-run composer update comment
    
    * Prefer dist for all composer calls
    
    * Fix php7.2 lowest dependencies build
    
    * Add comment making it obvious php mongodb adapter is pinned to lowest

---
+++ Tests/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace PHPUnit\Framework;
+
+$class = 'PHPUnit\Framework\TestCase';
+
+if (!class_exists($class)) {
+
+    abstract class TestCase extends \PHPUnit_Framework_TestCase
+    {
+    }
+}
